5020ddc29bd5c6304d1709a1e0d84d802877d1e4
ahmar husain <nucleotidz@hotmail.com>
2026-05-10 14:31:49 +0530

    Added Archi test

---
+++ src/Test/Architecture.Test/AbstractionTest.cs
@@ -0,0 +1,23 @@
+﻿namespace Architecture.Test
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    using NetArchTest.Rules;
+
+    using nucleotidz.token.limiter.abstraction;
+
+    public class AbstractionTest
+    {
+        [Fact]
+        public void Abstraction_assembly_should_not_have_class()
+        {
+            var result = Types.InAssembly(typeof(ITokenLimiter).Assembly)
+                .Should()
+                .NotBeClasses()
+                .GetResult();
+            Assert.True(result.IsSuccessful);
+        }
+    }
+}
---
+++ src/Test/Architecture.Test/DependencyTest.cs
@@ -0,0 +1,45 @@
+﻿namespace architecture.test
+{
+    using NetArchTest.Rules;
+
+
+
+    using Xunit;
+
+    public class DependencyTest
+    {
+        [Fact]
+        public void configuration_assesmbly_not_depend_on_main()
+        {
+            var result = Types.InNamespace("nucleotidz.token.limiter.configuration")
+                .Should()
+                .NotHaveDependencyOnAny("nucleotidz.token.limiter")
+                .GetResult().IsSuccessful;
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void abstraction_assesmbly_not_depend_on_main()
+        {
+            var result = Types.InNamespace("nucleotidz.token.limiter.abstraction")
+                .Should()
+                .NotHaveDependencyOnAny("nucleotidz.token.limiter")
+                .GetResult().IsSuccessful;
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void main_assesmbly_not_depend_on_abstraction()
+        {
+            var result = Types.InNamespace("nucleotidz.token.limiter")
+                .Should()
+                .HaveDependencyOnAny("nucleotidz.token.limiter.configuration")
+                .GetResult();
+
+            Assert.True(result.IsSuccessful);
+        }
+
+    }
+}
