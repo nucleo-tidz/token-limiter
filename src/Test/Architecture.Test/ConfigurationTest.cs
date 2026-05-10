e18e738a8b3b64f2c24471203ce8fc9b9d0b45ea
ahmar husain <nucleotidz@hotmail.com>
2026-05-10 15:40:37 +0530

    Final

---
+++ src/Test/Architecture.Test/ConfigurationTest.cs
@@ -0,0 +1,53 @@
+﻿namespace Architecture.Test
+{
+    using Mono.Cecil;
+
+    using NetArchTest.Rules;
+
+    using nucleotidz.token.limiter.configuration.Options;
+
+    public class ConfigurationTest
+    {
+        [Fact]
+        public void configuration_assembly_option_folder_should_have_classes()
+        {
+            var result = Types.InAssembly(typeof(RedisOption).Assembly)
+               .That().ResideInNamespace("nucleotidz.token.limiter.configuration.Options")
+               .Should().BeClasses()
+               .And().BePublic()
+                .And().HaveNameEndingWith("Option").GetResult();
+
+            Assert.True(result.IsSuccessful);
+        }
+        [Fact]
+        public void configuration_assembly_model_folder_should_have_classes()
+        {
+            var result = Types.InAssembly(typeof(RedisOption).Assembly)
+               .That().ResideInNamespace("nucleotidz.token.limiter.configuration.TokenLimitModel")
+               .Should()
+              .BePublic()
+              .And().HaveNameEndingWith("Model").GetResult();
+
+            Assert.True(result.IsSuccessful);
+        }
+        [Fact]
+        public void configuration_assembly_enum_folder_should_have_immutable()
+        {
+            var result = Types.InAssembly(typeof(RedisOption).Assembly)
+               .That().ResideInNamespace("nucleotidz.token.limiter.configuration.Enums")
+               .Should()
+              .MeetCustomRule(new EnumRule())
+              .GetResult();
+
+            Assert.True(result.IsSuccessful);
+        }
+
+        public class EnumRule : ICustomRule
+        {
+            public bool MeetsRule(TypeDefinition type)
+            {
+                return type.IsEnum;
+            }
+        }
+    }
+}
